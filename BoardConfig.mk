d721f4e3c7bd1e9bcf3ae7bb558b04959cafa608
jchanme <39323414+jchanme@users.noreply.github.com>
2022-07-24 10:57:34 +0800

    Update for Psyche

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,4 +48,4 @@ TARGET_OTA_ASSERT_DEVICE := psyche
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Inherit from the proprietary version
-include vendor/xiaomi/alioth/BoardConfigVendor.mk
+include vendor/xiaomi/psyche/BoardConfigVendor.mk
